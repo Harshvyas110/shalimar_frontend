2de5912010f21c00def53115ad6003f17131bf40
Harshvyas110 <158991878+Harshvyas110@users.noreply.github.com>
2026-06-13 13:27:30 -0400

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore ESLint errors during production builds as well
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
